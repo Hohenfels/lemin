7192c0c116cb5c00c85cac5bf368f27045b95243
Félicien <Hohenfels@MacBook-Pro-de-Felicien.local>
2017-05-13 23:10:03 +0200

    Parsing fini erreur sur start et end

--- src/get_links.c
+++ src/get_links.c
@@ -6,13 +6,13 @@
 /*   By: frenaud <frenaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 14:41:28 by frenaud           #+#    #+#             */
-/*   Updated: 2017/05/13 17:55:48 by frenaud          ###   ########.fr       */
+/*   Updated: 2017/05/13 23:09:45 by frenaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/lemin.h"
 
-t_link	*add_link(t_room *room)
+t_link	*create_link(t_room *room)
 {
 	t_link	*link;
 
@@ -23,46 +23,29 @@ t_link	*add_link(t_room *room)
 	return (link);
 }
 
-t_link	*fill_link(t_link *link, t_room *room)
-{
-	t_link	*ret;
-
-	ret = link;
-	if (ret == NULL)
-		ret = add_link(room);
-	else
-	{
-		ret->next = add_link(room);
-	}
-	return (ret);
-}
-
-t_link	*create_links(t_env *env, t_room *room)
+void	fill_link(t_room *r1, t_room *r2)
 {
 	t_link	*link;
-	t_pipe	*pipe;
 
-	link = room->link;
-	pipe = env->pipes;
-	while (pipe)
+	if ((link = r2->link) == NULL)
 	{
-		if (ft_strcmp(pipe->room_a->name, room->name) == 0)
-			link = fill_link(link, pipe->room_b);
-		if (ft_strcmp(pipe->room_b->name, room->name) == 0)
-			link = fill_link(link, pipe->room_a);
-		pipe = pipe->next;
+		r2->link = create_link(r1);
+		return ;
 	}
-	return (link);
+	while (link->next != NULL)
+		link = link->next;
+	link->next = create_link(r1);
 }
 
 void	get_links(t_env *env)
 {
-	t_room	*room;
+	t_pipe	*pipe;
 
-	room = env->rooms;
-	while (room)
+	pipe = env->pipes;
+	while (pipe)
 	{
-		room->link = create_links(env, room);
-		room = room->next;
+		fill_link(pipe->room_a, pipe->room_b);
+		fill_link(pipe->room_b, pipe->room_a);
+		pipe = pipe->next;
 	}
 }
